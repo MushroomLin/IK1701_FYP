28b449eaae6fcc2cdb8ce86a1f35a2b99ef2eaa3
MushroomLin <oneera.mushroom@gmail.com>
2017-10-11 22:17:29 +0800

    update version1.1

---
+++ version1/data_helper.py
@@ -0,0 +1,40 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+import re
+
+def remove_stop_words(sentence):
+    """
+    :param sentence: input string
+    :return: sentence after removing stop words
+    """
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(sentence)
+    filtered_words = [w for w in tokens if not w in stopwords.words('english')]
+    return " ".join(filtered_words)
+
+
+def clean_str(string, rm_stopwords = False):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string=string.lower()
+
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    if rm_stopwords:
+        string=remove_stop_words(string)
+        return string.strip()
+    else:
+        return string.strip()
--- version1/train.py
+++ version1/train.py
@@ -1,7 +1,38 @@
 import pandas as pd
 import sklearn
+import nltk
+import data_helper as dh
+from nltk.corpus import sentiwordnet as swn
+# Read data
 data=pd.read_csv('./stocknews/Combined_News_DJIA.csv')
+# Shape: [Date, Label, Top1-25 News]
+# Label: "1" when DJIA Adj Close value rose or stayed as the same;
+#        "0" when DJIA Adj Close value decreased.
+print(data.columns.values)
+# Divide data into train and test part
 train = data[data['Date'] < '2015-01-01']
 test = data[data['Date'] > '2014-12-31']
-print(train.shape)
-print(test.shape)
+num_train_samples=train.shape[0]
+num_test_samples=test.shape[0]
+
+# Do data pre-process
+# Only use Top1 of every day, Todo: add all news into string
+train_trans=[]
+for i in train.ix[:,2]:
+    i=dh.clean_str(i)
+    train_trans.append(nltk.word_tokenize(i))
+print(train_trans)
+test_trans=[]
+for i in test.ix[:,2]:
+    i=dh.clean_str(i)
+    train_trans.append(nltk.word_tokenize(i))
+print(test_trans)
+
+# Todo: Convert train/test_trans into sentiment matrix train_X, test_X
+
+
+train_label=train['label']
+test_label=test['label']
+clf=sklearn.svm.SVC()
+clf.fit(train_X,train_label)
+print('accuracy =',clf.score(test_X,test_label))

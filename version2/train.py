e5f5640548555d9c1903d961ee9fd5433d7a1fc5
Boyce <hk542922091@gmail.com>
2017-10-20 00:49:08 +0800

    Run version 2 to get train/test.csv

--- version2/train.py
+++ version2/train.py
@@ -5,10 +5,10 @@ import data_handler as dh
 from sklearn.svm import SVC
 from sklearn.metrics import (brier_score_loss, precision_score, recall_score,
                              f1_score,accuracy_score,confusion_matrix,classification_report)
-# from sklearn import datasets, neighbors, linear_model
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.naive_bayes import GaussianNB
-# from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn import datasets, neighbors, linear_model
+#from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 import csv
 
 topn_headlines = 20 # Select top 1 to topn_headlines as samples
@@ -47,45 +47,45 @@ Y_train = Y_label[:int(num_samples * train_sample_perc)]
 X_test = X_data_list[int(num_samples * train_sample_perc):]
 Y_test = Y_label[int(num_samples * train_sample_perc):]
 
-# # Write the training set into a csv file
-# csvfile = file('../stocknews/train.csv','wb')
-# writer = csv.writer(csvfile)
-# header = ['No.']
-# for i in range(1, topn_headlines+1):
-# 	header.append('Top'+str(i)+'_sent')
-# #header.append(dataframe_col) # Add sum_sent as a feature
-# header.append('Label')
-# writer.writerow(header)
-# for i in range(len(X_train)):
-# 	writer.writerow([i+1]+X_train[i]+[int(Y_train[i])])
-# csvfile.close()
+# Write the training set into a csv file
+csvfile = file('../stocknews/train.csv','wb')
+writer = csv.writer(csvfile)
+header = ['No.']
+for i in range(1, topn_headlines+1):
+	header.append('Top'+str(i)+'_sent')
+#header.append(dataframe_col) # Add sum_sent as a feature
+header.append('Label')
+writer.writerow(header)
+for i in range(len(X_train)):
+	writer.writerow([i+1]+X_train[i]+[int(Y_train[i])])
+csvfile.close()
 
-# # Write the testing set into a csv file
-# csvfile = file('../stocknews/test.csv', 'wb')
-# writer = csv.writer(csvfile)
-# writer.writerow(header)
-# for j in range(len(X_test)):
-# 	writer.writerow([j+1]+X_test[j]+[int(Y_test[j])])
-# csvfile.close()
+# Write the testing set into a csv file
+csvfile = file('../stocknews/test.csv', 'wb')
+writer = csv.writer(csvfile)
+writer.writerow(header)
+for j in range(len(X_test)):
+	writer.writerow([j+1]+X_test[j]+[int(Y_test[j])])
+csvfile.close()
 
 # SVM for testing
-clf = SVC()
-clf.fit(X_train,Y_train)
-Y_predict = clf.predict(X_test)
-print(Y_predict)
-print(classification_report(Y_predict,Y_test))
-print('Accuracy: ' + str(clf.score(X_test, Y_test)))
+# clf = SVC()
+# clf.fit(X_train,Y_train)
+# Y_predict = clf.predict(X_test)
+# print(Y_predict)
+# print(classification_report(Y_predict,Y_test))
+# print('Accuracy: ' + str(clf.score(X_test, Y_test)))
 
 # Build voting classifier for testing
-# logistic = LogisticRegression(random_state=1)
-# rf = RandomForestClassifier(random_state=1)
-# gnb = GaussianNB()
-# svm = SVC(random_state=1,probability=True)
+#logistic = LogisticRegression(random_state=1)
+rf = RandomForestClassifier(random_state=1)
+gnb = GaussianNB()
+svm = SVC(random_state=1,probability=True)
 
-# eclf1 = VotingClassifier(estimators=[
-#       ('lr', logistic),('gnb',gnb),('svm',svm)], voting='soft', weights=[1,1,1],
-#        flatten_transform=True)
-# eclf1 = eclf1.fit(X_train, Y_train)
-# Y_predict_vote = eclf1.predict(X_test)
-# print (classification_report(Y_predict_vote,Y_test))
-# print ("Accuracy: " + str(accuracy_score(Y_predict_vote, Y_test)))
+eclf1 = VotingClassifier(estimators=[
+      ('rf', rf),('gnb',gnb),('svm',svm)], voting='soft', weights=[1,1,1],
+       flatten_transform=True)
+eclf1 = eclf1.fit(X_train, Y_train)
+Y_predict_vote = eclf1.predict(X_test)
+print (classification_report(Y_predict_vote,Y_test))
+print ("Accuracy: " + str(accuracy_score(Y_predict_vote, Y_test)))

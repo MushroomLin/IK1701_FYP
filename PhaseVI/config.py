c98402e262b54bd584170cac385c5217f0c0ee03
Boyce <hk542922091@gmail.com>
2018-04-11 10:59:24 +0800

    fix bug in stock_lstm.py

--- PhaseVI/config.py
+++ PhaseVI/config.py
@@ -1,5 +1,5 @@
 DATASET = './prices.csv'
 COMPANY = 'AAPL' 
 TRAIN_RATIO = 0.8 # the proportion of training data versus the whole dataset
-TIME_SPAN = 30 # how many consecutive days to look back for predicting a day
+TIME_SPAN = 1 # how many consecutive days to look back for predicting a day
 PLOT_TRAIN = False # whether to plot training set
